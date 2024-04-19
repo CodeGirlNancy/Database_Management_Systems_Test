a3cfac8836d94ba46584c345a0a4ee9ffb915598
CodeGirlNancy <143062614+CodeGirlNancy@users.noreply.github.com>
2024-04-20 00:30:04 +0300

    Update and rename Oldest employee.sql to url images.png

--- Oldest employee.sql
+++
@@ -1,9 +0,0 @@
-SELECT e.name AS employee_name, e.hire_date, d.name AS department_name
-FROM employees e
-JOIN departments d ON e.department_id = d.id
-WHERE e.hire_date = (
-    SELECT MIN(e2.hire_date)
-    FROM employees e2
-    WHERE e2.department_id = e.department_id
-)
-GROUP BY d.name, e.name, e.hire_date;
